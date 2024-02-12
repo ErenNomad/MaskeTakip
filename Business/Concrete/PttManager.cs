720788aa7fb5d9c33b25ca0e0afd2cf56424ddfb
ereny <erenyoruk1903@gmail.com>
2024-02-12 15:19:24 +0300

    first commit

---
+++ Business/Concrete/PttManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    internal class PttManager
+    {
+        public class PttManager
+        {
+            public void GiveMask()
+            {
+
+            }
+        }
+    }
+}
---
+++ WorkAround/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+
+Vatandas vatandas1 = new Vatandas();
+SelamVer();
+SelamVer();
+SelamVer();
+SelamVer("Emre");
+
+
+
+
+static void SelamVer(string ad = "İsim Yok")
+{
+    Console.WriteLine("Merhabalaaar" + " " + ad);
+}
+
+
+
+
+public class Vatandas
+{
+
+}
